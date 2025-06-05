0233d45361a6159d5f0ed54f16e9425d5c79fcb7
atbviry <atbviry@gmail.com>
2025-06-05 15:18:04 +0200

    test integration material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { MaterialModule } from './material.module';
+//import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { AdminTemplateComponent } from './admin-template/admin-template.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //MaterialModule
+   MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
