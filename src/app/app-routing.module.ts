df88203b2b0a80dbbe522357dc9f4fca08014761
atbviry <atbviry@gmail.com>
2025-06-09 11:14:39 +0200

    création  des pages de navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './profil/profil.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentsComponent } from './students/students.component';
+import { PayementsComponent } from './payements/payements.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "profile", component: ProfilComponent },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "students", component: StudentsComponent },
+  { path: "payments", component: PayementsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/payements/payements.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payements',
+  standalone: false,
+  templateUrl: './payements.component.html',
+  styleUrl: './payements.component.css'
+})
+export class PayementsComponent {
+
+}
